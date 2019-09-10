93e8a6a09598972404359b0e6cab90de6d36ece3
Elja Suhonen <eljailari.suhonen@gmail.com>
2019-09-10 13:15:47 +0300

    add page with inputs

---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+import {useState} from "react" 
+export default () => {
+    const [name, setName] = useState("Elja Suhonen")    
+    const [email, setEmail] = useState("eljailari.suhonen@gmail.com")
+    return <form onSubmit={()=>{alert(JSON.stringify({name, email}))}}>
+        <h1>Nimi</h1>
+        <h2>{name}</h2>
+        <input type="text" onChange={(event)=>{
+            setName(event.target.value)
+        }}/>
+        <h1>Sähköposti</h1>
+        <h2>{email}</h2>
+        <input type="text" onChange={(event)=>{
+            setEmail(event.target.value)
+        }}/>
+        <button type="submit">Lähetä</button>
+    </form>
+}
